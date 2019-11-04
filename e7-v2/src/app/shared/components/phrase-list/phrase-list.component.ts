f9c001eb44a1d4a3b7c11171a544b65c360e72f9
Александр Кочкин <Alexander.Kochkin@artezio.com>
2019-11-04 19:02:15 +0400

    Added second supplement of the seventh example, where the login routing module is managed by its own login module.

---
+++ e7-v2/src/app/shared/components/phrase-list/phrase-list.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router'
+
+import {Phrase} from '../../interfaces/phrase'
+import { PhraseService } from '../../services/phrase.service'
+
+@Component({
+  selector: 'app-phrase-list',
+  templateUrl: './phrase-list.component.html',
+  styleUrls: ['./phrase-list.component.scss']
+})
+export class PhraseListComponent implements OnInit {
+
+  selectedId: number
+  phrases: Phrase[]
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute, // Добавлен
+    private phraseService: PhraseService
+  ) {}
+
+  // Обработчик событий изменения параметров адресной строки.
+  ngOnInit() { // Params добавлен в import.
+    this.activatedRoute.params.forEach((params: Params) => {
+      this.selectedId = +params['id']
+      this.phraseService // обращаемся к сервису
+        .getAll() // получаем Promise
+        .then(result => this.phrases = result) // как только
+          // Promise перейдёт в состояние resolved результат
+          // его работы присваиваем свойству phrases
+    })
+  }
+
+  // Для выделения выбранного элемента данный метод принимает
+  // фразу и возвращает булевое значение.
+  isSelected(phrase: Phrase) {
+    return phrase.id === this.selectedId
+  }
+
+  onSelect(selected: Phrase) {
+    // При клике по элементу списка перенаправляем
+    // пользователя по адресу /phrases/id
+    // Адрес с обязательным параметром указан в настройках
+    // маршрутизации в файле app.routes.ts
+    this.router.navigate(['phrase', selected.id])
+  }
+
+}
